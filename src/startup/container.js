98a3775813b12b9666834d9959d11d2e3ce9f658
Niver Chilatra <niver.chilatra@gmail.com>
2021-05-19 10:28:32 -0500

    ADD rutas y conectamos al servidor

---
+++ src/startup/container.js
@@ -0,0 +1,35 @@
+const { createContainer, asClass, asValue, asFunction } = require('awilix');
+
+//Config
+const config = require("../config");
+const app = require(".");
+
+//servicios
+const { HomeService } = require("../services");
+
+//controladores
+const { HomeController } = require("../controllers");
+
+//Rutas
+const {HomeRoutes } = require("../routes/index.routes");
+const Routes = require("../routes");
+
+const container = createContainer();
+
+container.register({
+    app: asClass(app).singleton(),
+    router: asFunction(Routes).singleton(),
+    config: asValue(config)
+})
+    .register({
+        HomeService: asClass(HomeService).singleton()
+    })
+    .register({
+        HomeController: asClass(HomeController.bind(HomeController)).singleton()
+    })
+    .register({
+        HomeRoutes: asFunction(HomeRoutes).singleton()
+    });
+
+module.exports = container;
+
